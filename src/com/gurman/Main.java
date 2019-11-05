80fbbc3dd6009ba28c721c6264bff3351e7045c2
Roman <gurman3211@gmail.com>
2019-11-05 15:20:48 +0300

    Update Main.java

--- src/com/gurman/Main.java
+++ src/com/gurman/Main.java
@@ -5,20 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         String[] arr = new String[20];
-        arr[0] = "google";
-        arr[1] = "google50";
-        arr[2] = "googleDop";
-        arr[3] = "similarweb";
-        arr[4] = "archive-org-spark";
-        arr[5] = "archive-org-calend";
-        arr[6] = "nethouse";
-        arr[7] = "googleImg";
-        arr[8] = "domain-status";
-        arr[9] = "proxy-checker";
-        arr[10] = "dns-revers";
-        arr[11] = "cubdomain-com";
-        arr[12] = "cubdomain-com2";
-        arr[13] = "telegram";
+        arr[0] = "site";
+        arr[1] = "site2";
+        arr[2] = "site3";
 
         Settings settings = new SettingsRelease(arr[0]);
         ProxyTool proxy = new ProxyTool(".\\res\\proxy.txt");
